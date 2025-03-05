dd5eb697906485f8d7dc9e63b4a3d0fa69965b14
kylerampal <79913444+kylerampal@users.noreply.github.com>
2025-03-05 18:46:31 +1100

    week1

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -7,12 +7,14 @@ layout(location = 0) out vec4 o_colour;	// output to colour buffer
 
 void main() {
    vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
-   float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
+   //float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
+   	vec2 v = abs(p - vec2(0.5, 0.5));
+	float d = max(v.x+ v.y, v.y+v.x);
   
-   if (d < 0.5) {
+   if (d < 0.2) {
       o_colour = vec4(u_colour, 1);
    }
    else {
-      o_colour = vec4(0,0,0,1); // BLACK
+      o_colour = vec4(0.2,0,0,1); // BLACK
    }
 }
